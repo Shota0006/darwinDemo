95eaee7cfcfb6e41a912cd38380d90c17a8696c7
Shota Iwamoto <Shota.iwamoto@generac.com>
2024-10-30 15:37:18 -0700

    Initial Commit

---
+++ DarwinDemo/DarwinDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarwinDemoApp.swift
+//  DarwinDemo
+//
+//  Created by Shota Iwamoto on 2024-10-30.
+//
+
+import SwiftUI
+
+@main
+struct DarwinDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
